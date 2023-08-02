2414debc1237bbd4c5d0a6c95b0939e7afb346dd
PierreAnders <pierre.untas@gmail.com>
2023-08-02 13:51:03 +0200

    Ecriture TestNombre1()

---
+++ RomanNumbersExam.Test/RomanNumbersExamTest.cs
@@ -0,0 +1,17 @@
+namespace RomanNumbersExam.Test
+{
+    public class RomanNumberExamTest
+    {
+        [Fact(DisplayName = "Etant donné le chiffre 1 " +
+                            "Quand je le convertis en nombre romains" + 
+                            "Alors j'obtiens I")]
+        public void TestNombre1()
+        {
+            const uint chiffreArabe = 1;
+
+            var nombreRomain = ConvertisseurNombresRomains.Convertir(chiffreArabe);
+
+            Assert.Equal("I", nombreRomain);
+        }
+    }
+}
